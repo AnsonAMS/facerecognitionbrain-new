cc8af94f48f627d534e5cd8ea8cd707f43114d43
AnsonAMS <anson_tohata@hotmail.com>
2021-02-17 23:22:52 +0100

    Update files

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
-import React { Component } from 'react';
+import React, { Component } from 'react';
+import Navigation from './components/Navigation/Navigation';
 import './App.css';
 
-class App() extends Component {
+class App extends Component {
   render() {
     return (
       <div className="App">
         <Navigation />
-        <Logo />
+        {/* <Logo />
         <ImageLinkForm />
-        <FaceRecognition />
+        <FaceRecognition /> */}
       </div>
     );
   }
